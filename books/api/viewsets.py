7d6413d6e2e40ee9ab65e01d9d21e8c6d81e0a74
André Silva <82832214+AndreLCSilva@users.noreply.github.com>
2022-05-18 17:12:54 -0300

    Delete books directory

--- books/api/viewsets.py
+++
@@ -1,7 +0,0 @@
-from books import models
-from rest_framework import viewsets
-from books.api import serializers
-
-class BooksViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.BooksSerializer
-    queryset = models.Books.objects.all()
